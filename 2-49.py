71e3fb6fd7b02aaad1327e21ccedcd6a22b5b748
Keegan Doyle <kdoyle7@students.towson.edu>
2026-09-07 19:15:16 -0400

    Universal Python Formatting

--- 2-49.py
+++ 2-49.py
@@ -1,27 +1,24 @@
 import numpy as np
 from scipy.optimize import linprog
 
-c=[-30,-20,-50]
+c = [-30, -20, -50]
 
-A=[
-    [-1,-1,-1],
-    [-1,0,0],
-    [0,-1,0],
-    [0,0,-1],
-    [1,0.5,0.333],
-    [2,3,5],
-    [4,2,7]
+A = [
+    [-1, -1, -1],
+    [-1, 0, 0],
+    [0, -1, 0],
+    [0, 0, -1],
+    [1, 0.5, 0.333],
+    [2, 3, 5],
+    [4, 2, 7],
 ]
 
-b=[0,-200,-200,-150,1500,4000,6000]
+b = [0, -200, -200, -150, 1500, 4000, 6000]
 
-Aeq=[
-    [2,-3,0],
-    [0,5,-2]
-]
-beq=[0,0]
-res = linprog(c,A_ub=A,b_ub=b,A_eq=Aeq,b_eq=beq,method="highs")
-z= 30*res.x[0] + 20*res.x[1] + 50*res.x[2]
+Aeq = [[2, -3, 0], [0, 5, -2]]
+beq = [0, 0]
+res = linprog(c, A_ub=A, b_ub=b, A_eq=Aeq, b_eq=beq, method="highs")
+z = 30 * res.x[0] + 20 * res.x[1] + 50 * res.x[2]
 if res.success:
     print("Optimization was successful.")
     print(f"Maximum profit= {z:.1f}")
